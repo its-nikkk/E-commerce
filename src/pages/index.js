a331f34f46102b58f1652e6674f1af85e3338224
Nikhil Kumar <134207321+its-nikkk@users.noreply.github.com>
2023-11-04 05:08:05 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from './Home';
+export { default as Products } from './Products';
+export { default as Product } from './Product';
+export { default as ContactPage } from './ContactPage';
+export { default as Cart } from './Cart';
+export { default as PageNotFound } from './PageNotFound';
